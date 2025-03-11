098aa1821892263da4e2ada001b1f10ba54bafcd
marl0nx <unbanbaum2@gmail.com>
2025-03-11 08:14:45 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,87 @@
+public class Main {
+
+    /**
+     * Erstellt einen zufälligen Großbuchstaben.
+     * @return Der zufällige Großbuchstabe.
+     */
+    private static String generateRandomCapitalLetter() {
+        return null;
+    }
+
+    /**
+     * Erstellt einen zufälligen Kleinbuchstaben.
+     * @return Der zufällige Kleinbuchstabe.
+     */
+    private static String generateRandomLetter() {
+        return null;
+    }
+
+    /**
+     * Erstellt ein zufälliges Sonderzeichen.
+     * @return Das zufällige Sonderzeichen.
+     */
+    private static String generateRandomSpecialCharacter() {
+        return null;
+    }
+
+    /**
+     * Erstellt eine zufällige Zahl.
+     * @return Die zufällige Zahl.
+     */
+    private static int generateRandomNumber() {
+        return 0;
+    }
+
+    /**
+     * Der Algorithmus, der das Passwort generiert.
+     * @param useSpecialCharacters Gibt an, ob Sonderzeichen verwendet werden sollen.
+     * @return Das sichere Passwort.
+     */
+    private static String pwGeneratorAlgorithm(boolean useSpecialCharacters) {
+        return null;
+    }
+
+    /**
+     * Generiert ein sicheres Passwort.
+     * @param useSpecialCharacters Gibt an, ob Sonderzeichen verwendet werden sollen.
+     * @return Das sichere Passwort.
+     */
+    private static String[] bulkGeneratePasswords(int amount, boolean useSpecialCharacters) {
+        return null;
+    }
+
+    /**
+     * Überprüft die Stärke des Passworts.
+     * @param password Das zu überprüfende Passwort.
+     * @return Die Stärke des Passworts. (0 = sehr schwach, 1 = schwach, 2 = mittel, 3 = stark)
+     */
+    private static int checkPwStrength(String password) {
+        return 0;
+    }
+
+    /**
+     * Exportiert das Passwort in eine Datei.
+     * @param path Der Pfad, in dem die Datei gespeichert werden soll.
+     * @param fileName Der Name der Datei.
+     */
+    private static void exportPassword(String path, String fileName) {
+
+    }
+
+    /**
+     * Liest die Attribute des Passworts aus einer Datei.
+     * @return Die Attribute des Passworts.
+     */
+    private static String[] readPasswordAttributes() {
+        return null;
+    }
+
+    /**
+     * Die Main-Methode des Programms.
+     * @param args Optionale Argumente.
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
